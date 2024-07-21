20d51e8ccd4a524293d4e165b918b4f378e181b0
someone5678 <someone5678@users.noreply.github.com>
2024-07-22 07:08:40 +0900

    GameSpace: Move game library to its own preference
    * And move it to top of root preference
    
    Change-Id: I0aeef9ac5f8808e3eacb140381a8c8abf35bf98e
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
@@ -17,38 +17,17 @@
  */
 package io.chaldeaprjkt.gamespace.settings
 
-import android.content.Intent
 import android.os.Bundle
-import android.provider.Settings
 import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import dagger.hilt.android.AndroidEntryPoint
 import io.chaldeaprjkt.gamespace.R
-import io.chaldeaprjkt.gamespace.preferences.AppListPreferences
-import io.chaldeaprjkt.gamespace.preferences.appselector.AppSelectorActivity
 import io.chaldeaprjkt.gamespace.preferences.QuickStartAppPreference
 import io.chaldeaprjkt.gamespace.preferences.QuickStartAppPreferenceDialogFragment
 
 @AndroidEntryPoint(PreferenceFragmentCompat::class)
 class SettingsFragment : Hilt_SettingsFragment() {
-
-    private var apps: AppListPreferences? = null
-
-    private val selectorResult =
-        registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) {
-            apps?.useSelectorResult(it)
-        }
-
-    private val perAppResult =
-        registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) {
-            apps?.usePerAppResult(it)
-        }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
@@ -56,23 +35,10 @@ class SettingsFragment : Hilt_SettingsFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        apps = findPreference(Settings.System.GAMESPACE_GAME_LIST)
-        apps?.onRegisteredAppClick {
-            perAppResult.launch(Intent(context, PerAppSettingsActivity::class.java).apply {
-                putExtra(PerAppSettingsActivity.EXTRA_PACKAGE, it)
-            })
-        }
-
-        findPreference<Preference>(AppListPreferences.KEY_ADD_GAME)
-            ?.setOnPreferenceClickListener {
-                selectorResult.launch(Intent(context, AppSelectorActivity::class.java))
-                return@setOnPreferenceClickListener true
-            }
     }
 
     override fun onResume() {
         super.onResume()
-        apps?.updateAppList()
     }
 
     override fun onDisplayPreferenceDialog(preference: Preference) {
